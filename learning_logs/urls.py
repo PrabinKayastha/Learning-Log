3fb6e1496a376b3c2ac85701efd83cf6c8a8e32e
Prabin Kayastha <princekayastha@gmail.com>
2020-02-10 15:28:49 +0545

    added url to the project

---
+++ learning_logs/urls.py
@@ -0,0 +1,11 @@
+"""Defines the URL pattern for learning_logs"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Homepage
+    path('', views.index, name='index'),
+]

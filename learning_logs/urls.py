179cdb66756e9ea21409a4aed8209cc474335e08
Prabin Kayastha <princekayastha@gmail.com>
2020-02-11 15:23:03 +0545

    added a page to display the Topics in the database

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,11 +1,13 @@
 """Defines the URL pattern for learning_logs"""
-
 from django.urls import path
-
 from . import views
+
 
 app_name = 'learning_logs'
 urlpatterns = [
     # Homepage
     path('', views.index, name='index'),
+
+    # Page that hows all topic.
+    path('topic/', views.topics, name='topics')
 ]

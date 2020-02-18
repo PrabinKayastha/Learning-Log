8b25c81bb94d34a81a9d83b9640d31e13ff19659
PrabinKayastha <princekayastha@gmail.com>
2020-02-18 11:44:49 +0545

    registered register url

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,14 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 
 urlpatterns = [
     # Include default auth URLs
     path('', include('django.contrib.auth.urls')),
+
+    # Registration Page
+    path('register/', views.register, name='register'),
 ]

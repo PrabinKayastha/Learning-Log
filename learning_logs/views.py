3af7e587db2cd45425c4f2399bf1522f0299fdf4
Prabin Kayastha <princekayastha@gmail.com>
2020-03-04 14:41:00 +0545

    added current user as owner of the topic added.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -42,7 +42,9 @@ def new_topic(request):
         # Post the data submitted and process the data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic= form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
 
     # Display a blank of invalid form.

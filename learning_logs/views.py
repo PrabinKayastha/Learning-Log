7773a17c05987613ae344dcc0222e8d51a22ef39
Prabin Kayastha <princekayastha@gmail.com>
2020-03-05 15:46:54 +0545

    added current user as the default owner of the new entry.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -66,6 +66,7 @@ def new_entry(request, topic_id):
         if form.is_valid():
             new_entry = form.save(commit=False)
             new_entry.topic = topic
+            new_entry.owner = request.user
             new_entry.save()
             return redirect('learning_logs:topic', topic_id=topic_id)
 
